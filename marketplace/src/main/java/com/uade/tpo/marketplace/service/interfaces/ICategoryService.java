b47fecd2fb7afa9b27735ed07037046788df388b
AgustinNari <agusnari21@gmail.com>
2025-09-10 14:46:51 -0300

    Limpieza

--- marketplace/src/main/java/com/uade/tpo/marketplace/service/interfaces/ICategoryService.java
+++ marketplace/src/main/java/com/uade/tpo/marketplace/service/interfaces/ICategoryService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-import com.uade.tpo.marketplace.entity.basic.Category;
 import com.uade.tpo.marketplace.entity.dto.create.CategoryCreateDto;
 import com.uade.tpo.marketplace.entity.dto.response.CategoryResponseDto;
 import com.uade.tpo.marketplace.exceptions.CategoryDuplicateException;
--- marketplace/src/main/java/com/uade/tpo/marketplace/service/interfaces/IDigitalKeyService.java
+++ marketplace/src/main/java/com/uade/tpo/marketplace/service/interfaces/IDigitalKeyService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.uade.tpo.marketplace.entity.basic.DigitalKey;
 import com.uade.tpo.marketplace.entity.dto.response.DigitalKeyResponseDto;
 import com.uade.tpo.marketplace.exceptions.DigitalKeyDuplicateException;
 import com.uade.tpo.marketplace.exceptions.InsufficientStockException;
